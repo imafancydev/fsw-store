83d57f322d9555e11093f35b1e1d29b4372c536c
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-03-14 17:34:49 -0300

    feat: add ProductItem and ProductList components

--- src/app/(home)/components/product-list.tsx
+++ src/app/(home)/components/product-list.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@prisma/client";
 import { ProductItem } from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
 interface ProductListProps {
   products: Product[];
 }
@@ -8,7 +9,10 @@ export const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="flex w-full gap-4 overflow-x-auto p-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
+        <ProductItem
+          key={product.id}
+          product={computeProductTotalPrice(product)}
+        />
       ))}
     </div>
   );
