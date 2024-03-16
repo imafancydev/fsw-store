14c11b3d80586166ec8068187a152da388872614
Lucas Augusto <lucasaugustoleon@gmail.com>
2024-03-16 00:40:24 -0300

    feat: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4">
+      <div className="text-[0.8rem] opacity-75">
+        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
